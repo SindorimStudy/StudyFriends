9823a2f54c524877623381c8909e0e740726beb4
soo <jong920224@naver.com>
2021-08-24 11:53:26 +0900

    Test

---
+++ styfriends/src/main/java/com/study/friends/StyfriendsApplication.java
@@ -0,0 +1,13 @@
+package com.study.friends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StyfriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StyfriendsApplication.class, args);
+	}
+
+}
