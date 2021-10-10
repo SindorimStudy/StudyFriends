a6c5bd72b5d495156c37ab779454715f86280d12
rachel5004 <j26597719@naver.com>
2021-10-10 18:33:37 +0900

    ADD:: naver oauth2

--- styfriends/src/main/java/com/study/friends/config/WebSecurityConfig.java
+++ styfriends/src/main/java/com/study/friends/config/WebSecurityConfig.java
@@ -1,11 +1,14 @@
 package com.study.friends.config;
 
 import com.study.friends.config.auth.CustomOAuthUserService;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
 import lombok.AllArgsConstructor;
@@ -16,27 +19,34 @@ import lombok.AllArgsConstructor;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	private final CustomOAuthUserService customOAuthUserService;
 
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 	@Override
     public void configure(WebSecurity webSecurity) throws Exception {
         // static 디렉터리의 하위 파일 목록은 인증 무시 ( = 항상통과 )
-        webSecurity.ignoring().antMatchers("/css/**", "/js/**", "/image/**", "/lib/**");
+        webSecurity.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+				.and().ignoring().mvcMatchers("/image/**");
     }
-	
+
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception{
 		httpSecurity
 		// h2-console 옵션 disable
         .csrf().disable().headers().frameOptions().disable()
         .and().authorizeRequests()
-			.antMatchers("/","/oauth2/**","/signin/**","/login/**","console/**","/h2-console/**")
+			.antMatchers("/","/oauth2/**","/signin/**","/login/**","/console/**","/h2-console/**")
 			.permitAll()
 		// 인증된 사용자만 접근 가능
 		.anyRequest().authenticated()
 		.and().exceptionHandling()
 		// 인증 없이 페이지에 접근할 경우 리다이렉트
+				// TODO: naver oauth 로그인 후 signin으로 감,, redirect url 찾아서 permitall에 추가
 		.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/signin"))
 		.and().logout().logoutSuccessUrl("/")
-		.and().oauth2Login().userInfoEndpoint().userService(customOAuthUserService);
+		.and().oauth2Login().defaultSuccessUrl("/").userInfoEndpoint().userService(customOAuthUserService);
 	}
 
 }
--- styfriends/src/main/java/com/study/friends/config/auth/OAuthAttributes.java
+++ styfriends/src/main/java/com/study/friends/config/auth/OAuthAttributes.java
@@ -29,6 +29,8 @@ public class OAuthAttributes {
     public static OAuthAttributes of(String registrationId,
                                      String userNameAttributeName,
                                      Map<String, Object> attributes) {
+        if( registrationId.equals("kakao")) return ofKakao(userNameAttributeName, attributes);
+        else if( registrationId.equals("naver")) return ofNaver(userNameAttributeName, attributes);
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -47,8 +49,19 @@ public class OAuthAttributes {
         return OAuthAttributes.builder()
                 .name((String) response.get("name"))
                 .email((String) response.get("email"))
-                .picture((String) response.get("picture"))
+                .picture((String) response.get("profile_image"))
                 .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+    public static OAuthAttributes ofKakao(String userNameAttributeName, Map<String,Object> attributes) {
+        Map<String,Object> kakao_account = (Map<String, Object>) attributes.get("kakao_account");
+        Map<String,Object> profile = (Map<String, Object>) kakao_account.get("profile");
+        return OAuthAttributes.builder()
+                .name((String) profile.get("nickname"))
+                .email((String) kakao_account.get("email"))
+                .picture((String) profile.get("profile_image_url"))
+                .attributes(attributes)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
